05bc5eedc25e9a851d98136ee6404d0b49567e10
Bin Qian <91174893+qianbin1989228@users.noreply.github.com>
2023-09-17 14:45:28 +0000

    change code

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,64 +1,20 @@
 ﻿#include "algorithm.h"
 
-//// cv::Mat转换成QImage
-//QImage cvMat2QImage(const Mat& mat)
-//{
-//    if (mat.type() == CV_8UC1)                          // 单通道
-//    {
-//        QImage image(mat.cols, mat.rows, QImage::Format_Indexed8);
-//        image.setColorCount(256);                       // 灰度级数256
-//        for (int i = 0; i < 256; i++)
-//        {
-//            image.setColor(i, qRgb(i, i, i));
-//        }
-//        uchar *pSrc = mat.data;                         // 复制mat数据
-//        for (int row = 0; row < mat.rows; row++)
-//        {
-//            uchar *pDest = image.scanLine(row);
-//            memcpy(pDest, pSrc, mat.cols);
-//            pSrc += mat.step;
-//        }
-//        return image;
-//    }
-//    else if (mat.type() == CV_8UC3)                     // 3通道
-//    {
-//        const uchar *pSrc = (const uchar*)mat.data;     // 复制像素
-//        QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);    // R, G, B 对应 0,1,2
-//        return image.rgbSwapped();                      // rgbSwapped是为了显示效果色彩好一些。
-//    }
-//    else if (mat.type() == CV_8UC4)                     // 4通道
-//    {
-//        const uchar *pSrc = (const uchar*)mat.data;     // 复制像素
-//        QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_ARGB32);        // B,G,R,A 对应 0,1,2,3
-//        return image.copy();
-//    }
-//    else
-//    {
-//        return QImage();
-//    }
-//}
+// cv::Mat转换成QImage
+// QImage cvMat2QImage(const Mat& mat)
+// {
+//     const uchar *pSrc = (const uchar*)mat.data;
+//     QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
+//     return image.rgbSwapped();
+// }
 
-//// QImage转换成cv::Mat
-//Mat QImage2cvMat(QImage image)
-//{
-//    Mat mat;
-//    switch (image.format())
-//    {
-//    case QImage::Format_ARGB32:
-//    case QImage::Format_RGB32:
-//    case QImage::Format_ARGB32_Premultiplied:
-//        mat = Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
-//        break;
-//    case QImage::Format_RGB888:
-//        mat = Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
-//        cv::cvtColor(mat, mat, CV_BGR2RGB);
-//        break;
-//    case QImage::Format_Indexed8:
-//        mat = Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
-//        break;
-//    }
-//    return mat;
-//}
+// QImage转换成cv::Mat
+// Mat QImage2cvMat(QImage image)
+// {
+//     Mat mat = Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
+//     cv::cvtColor(mat, mat, CV_BGRA2BGR);
+//     return mat;
+// }
 
 
 //证件照智能处理算法
